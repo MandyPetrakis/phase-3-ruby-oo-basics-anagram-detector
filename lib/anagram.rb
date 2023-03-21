393600cb298ce1a74fd04d3e1d24dc777d4c29f6
Mandy Petrakis <mandypetrakis@gmail.com>
2023-03-20 17:35:55 -0700

    commit tree d7c2c4c849ac9b2b5c39a657116a8635d3dfb75e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,8 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    def match array
+        array.filter{|w| w.chars.sort == @word.chars.sort }
+    end
+end
